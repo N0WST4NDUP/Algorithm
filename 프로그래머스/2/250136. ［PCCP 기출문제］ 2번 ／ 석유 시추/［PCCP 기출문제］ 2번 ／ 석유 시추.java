14fb03d222a0a304085ccdeb11a126f372828f23
N0WST4NDUP <153244723+N0WST4NDUP@users.noreply.github.com>
2024-02-16 16:54:39 +0900

    Update ［PCCP 기출문제］ 2번 ／ 석유 시추.java

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -9,40 +9,40 @@ public class Solution {
 	public int solution(int[][] land) {
 		int yLng = land.length;
 		int xLng = land[0].length;
-        this.oilId = new int[land.length][land[0].length];
-        int most = 0;
-        
-        Map<Integer, Integer> oilSize = new HashMap<>();
-        
-        for (int i=0; i<xLng; i++) {
-        	for (int j=0; j<yLng; j++) {
-                if (land[j][i] == 0) continue;
-                
-        		if (oilId[j][i] == 0) {
-        			int size = bfs(land,j,i);
-            		oilSize.put(id++,size);
-            	}
-        	}
-        }
-        
-        for (int i=0; i<xLng; i++) {
-        	List<Integer> column = new ArrayList<>();
-        	int size = 0;
-        	for (int j=0; j<yLng; j++) {
-        		if (land[j][i] > 0 && !column.contains(oilId[j][i])) {
-        			column.add(oilId[j][i]);
-        		}
-        	}
-        	
-        	for (int id : column) {
-        		size += oilSize.get(id);
-        	}
-        	
-        	most = Math.max(most, size);
-        }
-
-        return most;
-    }
+	        this.oilId = new int[land.length][land[0].length];
+	        int most = 0;
+	        
+	        Map<Integer, Integer> oilSize = new HashMap<>();
+	        
+	        for (int i=0; i<xLng; i++) {
+	        	for (int j=0; j<yLng; j++) {
+	                	if (land[j][i] == 0) continue;
+	                
+	        		if (oilId[j][i] == 0) {
+	        			int size = bfs(land,j,i);
+	            			oilSize.put(id++,size);
+	            		}
+	        	}
+	        }
+	        
+	        for (int i=0; i<xLng; i++) {
+	        	List<Integer> column = new ArrayList<>();
+	        	int size = 0;
+	        	for (int j=0; j<yLng; j++) {
+	        		if (land[j][i] > 0 && !column.contains(oilId[j][i])) {
+	        			column.add(oilId[j][i]);
+	        		}
+	        	}
+	        	
+	        	for (int id : column) {
+	        		size += oilSize.get(id);
+	        	}
+	        	
+	        	most = Math.max(most, size);
+	        }
+	
+	        return most;
+	    }
     
 	private int bfs(int[][] land, int y, int x) {
 		Queue<int[]> q = new LinkedList<>();
@@ -55,9 +55,7 @@ public class Solution {
 			for (int i = 0; i<4; i++) {
 				int ny = curr[0] + dy[i];
 				int nx = curr[1] + dx[i];
-				if (nx >= 0 && nx < land[0].length &&
-                ny >= 0 && ny < land.length &&
-                oilId[ny][nx] == 0 && land[ny][nx] == 1) {
+				if (nx >= 0 && nx < land[0].length && ny >= 0 && ny < land.length && oilId[ny][nx] == 0 && land[ny][nx] == 1) {
 					q.offer(new int[] {ny,nx});
 					oilId[ny][nx] = id;
 					size++;
