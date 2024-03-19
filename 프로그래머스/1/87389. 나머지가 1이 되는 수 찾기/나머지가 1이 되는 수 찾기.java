fedde87b2f13319fd68ac9462baababf9d56aef5
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-03-19 23:45:47 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 13.68 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        boolean[] prime = new boolean[n];
+        for (int i=2; i<n; i++) {
+            if (prime[i]) continue;
+            if (n%i == 1) return i;
+            for (int j = i*2; j<n; j+=i) {
+                if (prime[i]) continue;
+                prime[j] = true;
+            }
+        }
+        return -1;
+    }
+}
