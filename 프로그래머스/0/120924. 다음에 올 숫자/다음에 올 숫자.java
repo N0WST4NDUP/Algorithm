e0f53194ec36f5e287fc83f39b752a551dfe7d4d
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-04-19 20:17:37 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,6 +3,6 @@ class Solution {
         int a1 = common[1]-common[0];
         int a2 = common[2]-common[1];
         int idx = common.length-1;
-        return a1==a2 ? common[idx]+(common[idx]-common[idx-1]) : (int)Math.pow(common[idx],2)/common[idx-1] ;
+        return a1==a2 ? common[idx]+a1 : common[idx]*(common[1]/common[0]) ;
     }
 }
