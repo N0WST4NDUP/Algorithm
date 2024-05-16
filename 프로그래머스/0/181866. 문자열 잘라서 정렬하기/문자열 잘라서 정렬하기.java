ec2cda4d4f32973fe518614abf9e7f6c4ed32273
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-05-16 10:03:13 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 26.62 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String myString) {
+        String[] answer = myString.replaceAll("x+"," ").trim().split(" ");
+        Arrays.sort(answer);
+        return answer;
+    }
+}
