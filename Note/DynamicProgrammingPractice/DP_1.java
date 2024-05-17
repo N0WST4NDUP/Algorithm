cdcd33f511530de19de84ec8269dd6590e83bc6e
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-05-17 20:56:50 +0900

    Update DP_1.java

--- Note/DynamicProgrammingPractice/DP_1.java
+++ Note/DynamicProgrammingPractice/DP_1.java
@@ -10,7 +10,7 @@ public class DP_1 {
 		int n = sc.nextInt();
 		dp = new int[n+1];
 		long start = System.nanoTime();
-		System.out.println(dynamicFibo(n));
+		System.out.println(dynamicFibo2(n));
 		System.out.println("걸린 시간 : " + (System.nanoTime()-start) + "ns");
 		sc.close();
 	}
@@ -22,13 +22,28 @@ public class DP_1 {
 		if (x == 2 || x == 1) return 1;
 		return fibo(x-2) + fibo(x-1);
 	}
+	//TopDown (recursive)
 	//in -> 30
 	//out -> 832040
 	//time -> 81700ns
-	static int dynamicFibo(int x) {
+	static int dynamicFibo1(int x) {
 		if (x == 2 || x == 1) return 1;
 		if (dp[x] != 0) return dp[x];
-		dp[x] = dynamicFibo(x-2) + dynamicFibo(x-1);
+		dp[x] = dynamicFibo1(x-2) + dynamicFibo1(x-1);
+		return dp[x];
+	}
+	//BottomUp (loop)(for or while)
+	//in -> 30
+	//out -> 832040
+	//time -> 81200ns
+	static int dynamicFibo2(int x) {
+		int idx = 1;
+		dp[idx++] = 1;
+		dp[idx++] = 1;
+		while (idx <= x) {
+			dp[idx] = dp[idx-2] + dp[idx-1];
+			idx++;
+		}
 		return dp[x];
 	}
 }
