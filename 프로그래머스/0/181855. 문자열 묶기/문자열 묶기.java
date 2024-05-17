c2d98c6284cc13e8d0017e612702bc979464f01f
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-05-17 11:28:45 +0900

    [level 0] Title: 문자열 묶기, Time: 7.50 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] count = new int[31];
+        for (String str : strArr) {
+            count[str.length()]++;
+        }
+        return Arrays.stream(count).max().orElse(0);
+    }
+}
