c47a7a8e1e9222d2123a2bd1cbde467fd7db56c4
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-05-22 20:51:04 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.77 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String result) {
+        int[] cnt = new int[5];
+        int idx = 1;
+        for (char c : result.toCharArray()) {
+            switch (c) {
+                case 'S' : cnt[idx] = (int)Math.pow(cnt[idx], 1); idx++; break;
+                case 'D' : cnt[idx] = (int)Math.pow(cnt[idx], 2); idx++; break;
+                case 'T' : cnt[idx] = (int)Math.pow(cnt[idx], 3); idx++; break;
+                case '*' : cnt[idx-1] *= 2; cnt[idx-2] *= 2; break;
+                case '#' : cnt[idx-1] *= -1; break;
+                case '0' : cnt[idx] *= 10; break;
+                default : cnt[idx] = c-'0';
+            }
+        }
+        return Arrays.stream(cnt).sum();
+    }
+}
