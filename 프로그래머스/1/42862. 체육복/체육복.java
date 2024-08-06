06cfd1c8c86a8cb75d19c9174d7bc7fd296f8e3e
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-08-06 23:31:05 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] students = new int[n];
-        Arrays.fill(students, 1);
+        int answer = n;
+        int[] students = new int[n+2];
         
-        for (int i : lost) {
-            students[i-1]--;
-        }
+        for (int l : lost) students[l]--;
         
-        for (int i : reserve) {
-            students[i-1]++;
-        }
+        for (int r : reserve) students[r]++;
         
-        for (int i=0; i<students.length; i++) {
-            if (students[i] > 1) {
-                if (i > 0 && students[i-1] < 1) {
-                    students[i-1]++;
-                    students[i]--;
-                } else if (i < students.length-1 && students[i+1] < 1) {
-                    students[i+1]++;
-                    students[i]--;
+        for (int i=1; i<=n; i++) {
+            if (students[i] < 0) {
+                if (students[i-1] > 0) {
+                    students[i-1]--;
+                    students[i]++;
+                } else if (students[i+1] > 0) {
+                    students[i+1]--;
+                    students[i]++;
+                } else {
+                    answer--;
                 }
             }
         }
 
-        return (int)Arrays.stream(students).filter(i -> i > 0).count();
+        return answer;
     }
 }
