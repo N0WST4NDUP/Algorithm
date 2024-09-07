8b54296de9e8e5eadc5c0c5ff40a071ab54b626f
Jaeseo Lee <153244723+N0WST4NDUP@users.noreply.github.com>
2024-09-07 23:20:21 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        
+        while (Math.max(bill[0], bill[1]) > Math.max(wallet[0], wallet[1]) 
+              || Math.min(bill[0], bill[1]) > Math.min(wallet[0], wallet[1])) {
+            
+            bill[bill[0]>bill[1] ? 0 : 1] /= 2;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
